3a00b95cb7ffd751be7688397d22e36bf4fffed1
gregdevjs <iheanemegregory9@gmail.com>
2023-07-27 01:01:49 +0100

    added syncfusion

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { environment } from './environments/environment';
+
 
 import { AppModule } from './app/app.module';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+
+registerLicense(environment.syncFusionKey);
+
+
+
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
