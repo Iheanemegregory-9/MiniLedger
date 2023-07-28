4b8cba3b12ab921febb3238ec6c3561141f3a1a9
gregdevjs <iheanemegregory9@gmail.com>
2023-07-28 22:56:01 +0100

    connected register form to firebase auth

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Message } from 'primeng/api';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -11,17 +12,32 @@ export class RegisterComponent implements OnInit {
 
   messages!: Message[];
 
+  email!:string
+  password!:string;
+
   
-  constructor(private route: Router){}
+  constructor(private route: Router, private authService: AuthService){}
 
   ngOnInit(): void {
+
+
+   
+
   }
   
 
 
   
-  createAccount(){
-    this.route.navigate(['/verify'])
+  createAccount(email:string, password:string){
+
+    this.authService.createAccount(email, password).then((res)=>{
+
+      localStorage.setItem('user', res.user.uid)
+      console.log(res.user);
+    }, err =>{
+      console.log(err.message);
+    })
+    // this.route.navigate(['/verify'])
   }
 
 }
