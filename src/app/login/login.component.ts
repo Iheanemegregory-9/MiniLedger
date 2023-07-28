657862e721b44dad4df5c44acad48592fc9d7c0f
gregdevjs <iheanemegregory9@gmail.com>
2023-07-28 22:55:39 +0100

    connected login form to firebase auth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Message } from 'primeng/api';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -11,8 +12,11 @@ export class LoginComponent implements OnInit {
 
   messages!: Message[];
 
+  email!:string
+  password!:string;
+
   
-  constructor(private route: Router){}
+  constructor(private route: Router, private authService: AuthService){}
 
   ngOnInit(): void {
   }
@@ -20,7 +24,12 @@ export class LoginComponent implements OnInit {
 
 
   
-  login(){
+  login(email:string, password:string){
+    this.authService.signIn(email, password).then((res)=>{
+      localStorage.setItem('user', res.user.uid);
+      console.log(res.user);
+      
+    })
     this.messages = [{ severity: 'success', summary: 'Success', detail: 'Message Content' }];
     // this.route.navigate(['/dashboard'])
   }
