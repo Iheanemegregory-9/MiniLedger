c82293bb25e44c33fd32443fd223d1888d6c98e4
gregdevjs <iheanemegregory9@gmail.com>
2023-08-12 15:33:02 +0100

    updated code to get and store firsName and lastName field

--- src/app/shared/income.service.ts
+++ src/app/shared/income.service.ts
@@ -1,24 +1,42 @@
-import { DatePipe } from '@angular/common';
 import { Injectable } from '@angular/core';
-import { Firestore, addDoc, collection, collectionData } from '@angular/fire/firestore';
+import { Firestore, addDoc, collection, collectionData, doc, getDoc, updateDoc } from '@angular/fire/firestore';
+import { Auth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IncomeService {
 
-  constructor(private firestore: Firestore) { }
+  constructor(private firestore: Firestore, private auth: Auth) { }
 
   addNewIncome(description:string, source:string, price:number, date:Date){
 
-    const colRef = collection(this.firestore, 'Income');
+    const colRef = collection(this.firestore, 'User');
     return addDoc(colRef, {description, source, price, date})
   }
 
-  getIncomeData(id:string){
+  getIncomeData(){
     const colRef = collection(this.firestore, 'Income');
-    return collectionData(colRef, {idField: id})
+    return collectionData(colRef, {idField: 'id'})
   }
+
+  getIncomeID(id:string){
+    const docRef = doc(this.firestore, `Income/${id}`)
+    const docSnap = getDoc(docRef);
+    return docSnap;
+  }
+ 
+
+  // User specific income <<<<< The code below allows user to read and write thier own income without affecing other users code
+
+
+  getUserData(){
+    const colRef = collection(this.firestore, 'Users');
+    return collectionData(colRef, {idField: 'id'});
+  }
+
+
+
 
   
 }
