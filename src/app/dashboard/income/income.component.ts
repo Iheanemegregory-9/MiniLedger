e4b6de39b91594c9249322b1c8eb8404e5f73797
gregdevjs <iheanemegregory9@gmail.com>
2023-08-19 00:43:45 +0100

    Fixed user details bug

--- src/app/dashboard/income/income.component.ts
+++ src/app/dashboard/income/income.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit, } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ServiceService } from 'src/app/shared/service.service';
 import { PageSettingsModel } from '@syncfusion/ej2-angular-grids';
@@ -6,12 +6,12 @@ import { FilterSettingsModel } from '@syncfusion/ej2-angular-grids';
 import { EditSettingsModel, ToolbarItems } from '@syncfusion/ej2-angular-grids';
 import { IncomeService } from 'src/app/shared/income.service';
 import { AuthService } from 'src/app/shared/auth.service';
+import { Auth, onAuthStateChanged } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-income',
   templateUrl: './income.component.html',
   styleUrls: ['./income.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class IncomeComponent implements OnInit {
 
@@ -20,7 +20,7 @@ export class IncomeComponent implements OnInit {
   public editSettings?: EditSettingsModel;
   public toolbar?: ToolbarItems[];
 
-  constructor(private incomeService: IncomeService, private activatedRoute: ActivatedRoute, private authService : AuthService){
+  constructor(private incomeService: IncomeService, private activatedRoute: ActivatedRoute, private authService : AuthService, private auth: Auth){
 
   }
 
@@ -34,23 +34,28 @@ export class IncomeComponent implements OnInit {
   itemID!:any;
   editIsHidden = true;
   firstName!:any;
-  userID!:any;
-  userDetails:any;
+  userID:any;
+  userDetails: any = {};
+  isVerified!:boolean;
 
   dataDetails:any;
 
 
   ngOnInit(): void {
 
-    this.activatedRoute.paramMap.subscribe(param =>{
-      this.userID = param.get('id')
-      this.loadUserData()
+    this.auth.onAuthStateChanged((user) => {
+      if(user){
+        this.userID = user.uid
+        console.log(user.uid);
+        this.getUserDetails(this.userID) 
+      }
+      else{
+        console.log('no user');
+      }
     })
-    this.loadIncomeData()
-    
-    // this.authService.currentUser()
+    this.loadIncomeData();
   }
-
+  
 
   openForm(){
     this.visible = true;
@@ -90,24 +95,48 @@ export class IncomeComponent implements OnInit {
    this.editIsHidden = false;
   }
 
-  loadUserData() {
-    this.authService.currentUser().subscribe(res =>{
-      this.userID = res?.uid
-      this.getUserDetails(this.userID);
+
+  getUserDetails(id:string) {
+    this.auth.onAuthStateChanged((user) => {
+      if(user){
+        this.userID = user.uid
+        console.log(user.uid);  
+        this.incomeService.getUserData(id).then(res =>{
+          this.userDetails = res.data()
+          console.log(res.data());
+        })
+      }
+      else{
+        console.log('no user');
+      }
     })
+} 
+
+
+  isEmailVerified(){
+    const verified = this.authService.currentUser()?.emailVerified
+    if(verified){
+        this.isVerified = true
+        } else if(!verified){
+        this.isVerified = false
+       } 
   }
 
-  getUserDetails(id: string) {
-    this.incomeService.getUserData(id).then(res => {
-      if (res.exists()) {
-        this.userDetails = res.data();
-        console.log(this.userDetails);
-      } else {
-        console.log('User not found');
-      }
-    }).catch(error => {
-      console.error('Error fetching user data:', error);
-    });
-  } 
+ 
+
+  // userStateChanged() {
+  //   this.auth.onAuthStateChanged((user) => {
+  //     if(user){
+  //       this.userID = user.uid
+  //       console.log(user.uid);  
+  //     }
+
+  //     else{
+  //       console.log('no user');
+  //     }
+  //   })
+
+  // }
+
 
 }
