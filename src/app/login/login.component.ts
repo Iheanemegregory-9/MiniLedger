33f6479d9d44a22b7ef5cb19fa8b115fab0198f9
gregdevjs <iheanemegregory9@gmail.com>
2023-08-20 10:32:12 +0100

    added form validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Message } from 'primeng/api';
+import { Message, MessageService } from 'primeng/api';
 import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [MessageService]
 })
 export class LoginComponent implements OnInit {
 
@@ -15,24 +16,57 @@ export class LoginComponent implements OnInit {
   email!:string
   password!:string;
 
+  loading:boolean = false;
+
+  btnMessage = "Sign in"
+
+  localEmail:any;
+  isAuth:any;
+  errMessage!:string;
+
   
-  constructor(private route: Router, private authService: AuthService){}
+  constructor(private route: Router, private authService: AuthService, private messageService: MessageService){}
 
   ngOnInit(): void {
+    this.authService.currentUser()
   }
   
+  show() {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'User logged in' });
+}
 
+showErrMessage(){
+  this.messageService.add({severity: 'error', summary: 'Opps', detail: this.errMessage})
+}
+
+changeBtnState(){
+  this.loading = true;
+  this.btnMessage = "Logging you in..."
+}
 
   
-  login(email:string, password:string){
+   login(email:string, password:string){
+    this.changeBtnState()
     this.authService.signIn(email, password).then((res)=>{
       localStorage.setItem('user', res.user.uid);
+      this.localEmail = res.user.email
       console.log(res.user);
-
-      this.route.navigate(['/'])
-      
+      localStorage.setItem('email', this.localEmail);
+      this.route.navigate(['/']);
+      this.show()
+    }, err =>{
+      if(err.message == 'Firebase: Error (auth/user-not-found).'){
+        this.errMessage = 'User with the specified email does not exists'
+      }else if (err.message == 'Firebase: Error (auth/wrong-password).'){
+        this.errMessage = 'Wrong password'
+      }else{
+        this.errMessage = 'Something went wrong'
+      }
+      console.log(err.message);
+      this.showErrMessage()
+      this.loading = false
+      this.btnMessage = 'Sign in'
     })
-    // this.messages = [{ severity: 'success', summary: 'Success', detail: 'Message Content' }];
-    // this.route.navigate(['/dashboard'])
+    this.loading = false
   }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,69 +1,82 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Message } from 'primeng/api';
+import { Message, MessageService } from 'primeng/api';
 import { AuthService } from '../shared/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [MessageService]
 })
 export class RegisterComponent implements OnInit {
 
   messages!: Message[];
-
-  firstName:string = '';
-  lastName:string = '';
-  email!:string
+  loading:boolean = false;
+  firstName!:string 
+  lastName!:string 
+  email!:any
   password!:string; 
   userID!: string
   userIsAnon!:boolean;
+  btnMessage = 'Create an Account'; 
+  errorMessage:any;
 
   
-  constructor(private route: Router, private authService: AuthService){}
+  constructor(private route: Router, private authService: AuthService, private messageService: MessageService){}
 
   ngOnInit(): void {
-
-
-   
-
   }
+
+  show() {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Your account has been created' });
+  }
+
+  showErrMessage() {
+    this.messageService.add({ severity: 'error', summary: 'Opps', detail: this.errorMessage });
+  }
+
+
+  changeBtnStatus(){
+    this.loading = true;
+    this.btnMessage = 'Creating your account'
+  }
+
   
-
-
-  
- async createAccount(email:string, password:string, firstName:string, lastName:string, id:string){
-
-    // this.authService.sendEmailVerification(email)
+ async createAccount(email:string, password:string, firstName:any, lastName:string,  id:string){
+  this.changeBtnStatus()
     await this.authService.createAccount(email, password).then((res)=>{
       this.userID = res.user.uid;
-        this.userIsAnon = res.user.isAnonymous;
-      localStorage.setItem('user', res.user.uid)
-      console.log(res.user)
-
-
-      
+      localStorage.setItem('user', res.user.uid);
     }, err =>{
       console.log(err.message);
+      if(err.message == 'Firebase: Error (auth/email-already-in-use).'){
+        this.errorMessage = 'A user with this email already exists'
+      }else if (err.message = 'Firebase: Password should be at least 6 characters (auth/weak-password).'){
+        this.errorMessage = 'Password should be at least 8 characters'
+      }else{
+        this.errorMessage = 'Something went wrong'
+      }
+      this.showErrMessage()
+      this.loading = false;
     })
-    
-    this.authService.setUserDetails(firstName, lastName, this.userID).then(res =>{
-
-      this.firstName = firstName;
-      this.lastName = lastName;
-      
-      console.log('hello ' + `${this.firstName} ` + `${this.lastName}`);
-      console.log(res);
+   await this.authService.setUserDetails(firstName, email, lastName, this.userID).then(res =>{
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email
     }, err =>{
       console.log(err.message);
-      
+      this.errorMessage = err.message
     })
+    this.loading = false;
 
-    
-
+    this.show()
    await this.route.navigate(['/'])
   }
 
   
 
 }
+
+
